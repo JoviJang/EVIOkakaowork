04cbcc0fd6b1f8d38ee6f9a870593ead7f25c0de
DESKTOP-Jovi\USER <jovi.jang@evio.kr>
2021-05-27 16:49:13 +0900

    시간포멧변경 및 몰트자동계산

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ nunjucks.configure('views', {
 });
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   resave: false,
